96d51d75d78f0c9716096630b5cdad7f69442448
wejdan <jode200951@hotmail.com>
2023-02-25 12:28:57 +0300

    Chat screen

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,14 +1,56 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect, useLayoutEffect, useCallback} from 'react';
+import {GiftedChat} from 'react-native-gifted-chat';
+import colors from '../theme/colors';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {TouchableOpacity, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {View, Image, StyleSheet} from 'react-native';
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
+  const onSignOut = () => {};
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10,
+          }}
+          onPress={onSignOut}>
+          <AntDesign
+            name="logout"
+            size={24}
+            color={colors.gray}
+            style={{marginRight: 10}}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+  const onSend = msgs => {
+    setMessages([...messages, ...msgs]);
+  };
   return (
-    <View>
-      <Text>Chat</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={false}
+      showUserAvatar={false}
+      onSend={messages => onSend(messages)}
+      messagesContainerStyle={{
+        backgroundColor: '#fff',
+      }}
+      textInputStyle={{
+        backgroundColor: '#fff',
+        borderRadius: 20,
+      }}
+      user={{
+        _id: 1,
+        avatar: 'https://i.pravatar.cc/300',
+      }}
+    />
   );
 };
 
 export default Chat;
-
-const styles = StyleSheet.create({});
